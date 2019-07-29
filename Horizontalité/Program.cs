cf0f32a72a3f133e6b227797ede16c0c0fbd0acc
Mathieu Scolas <mathieu.scolas@gmail.com>
2019-07-29 07:55:55 +0200

    add horizontal vs vertical fun

---
+++ Horizontalité/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Horizontalité
+{
+    class Item {
+        public string Name {get;set;}
+        public int Value {get;set;}
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static int HorizontalFunction() {
+            var items = GetItems();
+            return items.Where(it => it.Name.Contains("Unallowed Name")).Where(it=> it.Value < 0).GroupBy(it => it.Name).Max(grp => grp.Max(it => it.Value));
+        }
+
+        static int VerticalFunction() {
+            var items = GetItems();
+            return items
+                .Where(it => it.Name.Contains("Unallowed Name"))
+                .Where(it=> it.Value < 0)
+                .GroupBy(it => it.Name)
+                .Max(grp => grp.Max(it => it.Value));
+        }
+
+        static private IEnumerable<Item> GetItems() {
+            return new Item[0];
+        }
+    }
+}
