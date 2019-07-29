1954750135c54aa3b51c6af0d4bbb62f6f1cc37e
Mathieu Scolas <mathieu.scolas@gmail.com>
2019-07-29 18:12:56 +0200

    Update PPT + proximite code

---
+++ Proximite/Proximite.ts
@@ -0,0 +1,15 @@
+
+// Moi développeur junior (true story)
+var arrayCountryName: Array<string> = ["Belgique", "Fance", "Allemagne"]
+var arrayPopulation: Array<number> = [11350000, 67000000, 82790000]
+// for loop sur 2 array, croisong les doigts que rien ne s'est insérer.
+
+
+// Moi développeur senior
+type Country = { Name: string, Population: number }
+const coutry: Array<Country> = [
+    { Name: "Belgique", Population: 11350000 },
+    { Name: "Fance", Population: 67000000 },
+    { Name: "Allemagne", Population: 82790000 }
+];
+// for loop sur 1 array. Pas de confusion possible.
