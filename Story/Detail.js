0c7c5d2d95403e45717dd84e19706254795d7e16
Mathieu Scolas <mathieu.scolas@gmail.com>
2019-08-01 15:23:50 +0200

    add story part

---
+++ Story/Detail.js
@@ -0,0 +1,30 @@
+function decode(sentence) {
+    return sentence
+        .split(';')
+        .map(w => w.replace('&#', ''))
+        .map(w => parseInt(w, 10))
+        .map(i => String.fromCharCode(i))
+        .join('');
+}
+
+let sentence = '&#72;&#101;&#108;&#108;&#111;&#32;&#119;&#111;&#114;&#108;&#100;&#33;';
+let decodedSentence = decode(sentence);
+console.log(decodedSentence);
+
+
+let removeBalise = (word) => word.replace('&#', '');
+let toInt = (word) => parseInt(word, 10);
+let toAscii = (integer) => String.fromCharCode(integer);
+
+function decodeReadable(sentence) {
+    return sentence
+        .split(';')
+        .map(removeBalise)
+        .map(toInt)
+        .map(toAscii)
+        .join('');
+}
+
+let sentence = '&#72;&#101;&#108;&#108;&#111;&#32;&#119;&#111;&#114;&#108;&#100;&#33;';
+let decodedSentence = decodeReadable(sentence);
+console.log(decodedSentence);
