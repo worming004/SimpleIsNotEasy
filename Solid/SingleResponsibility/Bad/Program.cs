ddc6f03373d29c5ce45dcecf20502ccb31656ed4
Mathieu Scolas <worming@hotmail.be>
2022-10-20 20:04:35 +0200

    change target + move to good and bad

---
+++ Solid/SingleResponsibility/Bad/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SingleResponsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var allLogic = new AllTheLogic();
+            allLogic.ReadAndWrite();
+        }
+    }
+}
